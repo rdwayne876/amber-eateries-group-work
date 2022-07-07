c630c5d2a9e2c06d54f42d8c736d4989359b20a5
rdwayne876 <63379431+rdwayne876@users.noreply.github.com>
2022-07-07 14:29:06 -0500

    Database Schema update

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,10 +1,12 @@
-enum Category {
-    MAIN = 'main',
+export enum Category {
+    APPETIZER = 'appetizer',
+    ENTREE = 'entree',
+    DESSERT = 'dessert',
     SIDE = 'side',
     BEVERAGE = 'beverage',
-}
-
-export interface Product {
+  }
+  
+  export interface Product {
     id: number;
     name: string;
     description: string;
@@ -12,4 +14,5 @@ export interface Product {
     imageUrl: string;
     price: number;
     quantity: number;
-}
+    ratings: number[];
+  }
