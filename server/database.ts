7f3f8bf92e430fc66f3671f62d0969223e31e66f
SimonDMaxwell <lolicongress@outlook.com>
2022-07-07 12:51:50 -0500

    database.ts updated

--- server/database.ts
+++ server/database.ts
@@ -1,34 +1,52 @@
 import { Order, Transaction, User } from '../src/app/interfaces/checkout';
+import { Product, Category } from '../src/app/product';
 
-let database: { orders: Order[]; transactions: Transaction[]; users: User[] } =
-    { orders: [], transactions: [], users: [] };
+let database: {
+  products: Product[];
+  orders: Order[];
+  transactions: Transaction[];
+  users: User[];
+} = { products: [], orders: [], transactions: [], users: [] };
 
-//Data Entry Begin
+// Data Entry Begin
+database.products = [
+  // {
+  //     id: number;
+  //     name: string;
+  //     description: string;
+  //     category: 'main'|'side'|'beverage';
+  //     imageUrl: "https://source.unsplash.com/1600x900/?food";
+  //     price: number;
+  //     quantity: number;
+  //     ratings: []
+  // }
+];
 database.orders = [
-    // {
-    //     id: number;
-    //     orders: Product[];
-    //     order_note: string;
-    // }
+  // {
+  //     id: number;
+  //     orders: Product[];
+  //     order_note: string;
+  // }
 ];
 database.transactions = [
-    // {
-    //     id: number;
-    //     user_id: string;
-    //     payment_method: string;
-    //     payment_amount: number;
-    //     delivery: boolean;
-    //     address: Address;
-    // }
+  // {
+  //     id: number;
+  //     user_id: string;
+  //     payment_method: string;
+  //     payment_amount: number;
+  //     delivery: boolean;
+  //     address: Address;
+  // }
 ];
 database.users = [
-    // {
-    //     id: number;
-    //     email: string;
-    //     delivery_address: Address;
-    //     card: Card;
-    // }
+  // {
+  //     id: number;
+  //     email: string;
+  //     delivery_address: Address;
+  //     card: Card;
+  // }
 ];
-//Data Entry End
+// Data Entry End
 
 console.log(JSON.stringify(database));
+
