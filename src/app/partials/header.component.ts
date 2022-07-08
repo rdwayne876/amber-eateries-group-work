b165c559f15c76b7ddff002aaa4349108a4423f2
dev-apiarist <klesliedev@gmail.com>
2022-07-07 20:43:58 -0500

    landing-page-v2

--- src/app/partials/header.component.ts
+++ src/app/partials/header.component.ts
@@ -4,7 +4,10 @@ import { Component, OnInit } from '@angular/core';
     selector: 'app-header',
     template: `
         <header>
-            <div class="header">
+            <div
+                class="header shadow-2xl bg-gradient-to-r from-[#2b2a2a38] via-[#00000044] to-[#00000048]"
+                id="main-nav"
+            >
                 <div class="secondary-header">
                     <div class="main-container">
                         <div class="s-h-inner">
@@ -36,11 +39,15 @@ import { Component, OnInit } from '@angular/core';
                 </div>
                 <mat-toolbar>
                     <div class="main-container">
-                        <div class="m-h-inner">
+                        <div class="m-h-inner ">
                             <div class="logo">
-                                <a href="/"><span>Amber Eateries</span></a>
+                                <a href="/"
+                                    ><span style="color:white;"
+                                        >Amber Eateries</span
+                                    ></a
+                                >
                             </div>
-                            <div class="navigation">
+                            <div class="navigation ">
                                 <button mat-button routerLink="/">Home</button>
                                 <button mat-button routerLink="/menu">
                                     Menus
@@ -52,7 +59,7 @@ import { Component, OnInit } from '@angular/core';
                                     About
                                 </button>
                                 <button mat-button routerLink="/addproduct">
-                                    Add New Product
+                                    <i class="fa-solid fa-cart-shopping"></i>
                                 </button>
                             </div>
                         </div>
@@ -64,12 +71,11 @@ import { Component, OnInit } from '@angular/core';
     styles: [
         `
             @import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600&display=swap');
+
             .header {
                 width: 100%;
                 height: auto;
-                border-bottom: 1px solid #e4e7eb;
             }
-
             .main-container {
                 width: 78%;
                 margin: 0 auto;
@@ -78,7 +84,7 @@ import { Component, OnInit } from '@angular/core';
             .secondary-header {
                 width: 100%;
                 height: auto;
-                background-color: #00b5ad;
+                background-color: #6a472d;
                 font-size: 14px;
                 color: #fff;
             }
@@ -116,7 +122,7 @@ import { Component, OnInit } from '@angular/core';
                 height: 88px;
                 justify-content: space-between;
                 padding: 0 !important;
-                background-color: #fff;
+                background-color: transparent;
             }
 
             ::ng-deep .mat-button:last-child {
@@ -134,7 +140,7 @@ import { Component, OnInit } from '@angular/core';
             }
 
             .navigation ::ng-deep .mat-button {
-                color: #2b2b2b;
+                color: white;
             }
 
             .navigation ::ng-deep .mat-button:hover {
