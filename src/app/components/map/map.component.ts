e470b706218e4c306e115b0450257096eb71bb4b
SimonDMaxwell <lolicongress@outlook.com>
2022-07-08 23:10:32 -0500

    Map logic alteration and improvement for usability

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,49 +1,74 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { MapService } from '../../map.service';
+import { Component, AfterViewInit,Input  } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import * as L from 'leaflet';
+import { CheckoutService } from 'src/app/services/checkout.service';
+import { CartService } from 'src/app/cart.service';
+import { MapService } from '../../map.service';
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+    selector: 'app-map',
+    templateUrl: './map.component.html',
+    styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements AfterViewInit {
-  private map: any;
-  private data: any = [];
-  lat: any;
-  lon: any;
+    private map: any;
+    private data: any = [];
+    lat: any;
+    lon: any;
+    private marker: any;
 
+    // @Input('appMap') set refresh(value: boolean) {
+    //   this.setMapLocation();
+    //   // this.refresh = value;
+    // }
 
-  constructor(private mapService: MapService) { }
+    constructor(
+        private mapService: MapService,
+        public checkoutService: CheckoutService
+    ) {
+      
+    }
+    
 
+    initMap(): void {
+        this.map = L.map('map', {
+            zoom: 10,
+        });
 
-  private initMap(): void {
-    this.mapService.sendGetRequest().subscribe((data: any[]) => {
-      this.data = data
-      this.lat = this.data.data[0].latitude
-      this.lon = this.data.data[0].longitude
+        const tiles = L.tileLayer(
+            'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {
+                maxZoom: 18,
+                minZoom: 3,
+                attribution:
+                    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+            }
+        );
+        tiles.addTo(this.map);
 
-      this.map = L.map('map', {
-        center: [this.lat, this.lon],
-        zoom: 10
-      });
+        this.setMapLocation();
+    }
 
-      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        minZoom: 3,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      });
+    setMapLocation() {
+        this.mapService.getMap().subscribe((data: any[]) => {
+            this.data = data;
+            this.lat = this.data.data[0].latitude ?? 17.96795;
+            this.lon = this.data.data[0].longitude ?? -76.87128;
 
-      tiles.addTo(this.map);
-      const marker = L.marker([this.lat, this.lon])
-      marker.addTo(this.map)
-    })
+            this.addMarker();
+        });
+    }
 
+    private addMarker(): void {
+        if (this.marker) this.marker.remove();
 
-  }
+        this.marker = L.marker([this.lat, this.lon]);
+        this.marker.addTo(this.map);
 
-  ngAfterViewInit(): void {
-    this.initMap();
-  }
+        this.map.panTo([this.lat, this.lon]);
+    }
 
+    ngAfterViewInit(): void {
+        this.initMap();
+    }
 }
--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,21 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Address } from './interfaces/checkout';
   
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
-  private mapkey = '065ef2421aed6d90a7bb73ee2e532679'
+  private mapkey = '065ef2421aed6d90a7bb73ee2e532679';
   //default address should be changed depending on the user input
-  private address = '224 2 East Greater Portmore, Saint Catherine'
-  private apiUrl = `http://api.positionstack.com/v1/forward?access_key=${this.mapkey}&query=${this.address}`
+  address: Address = {street_address: "224", street_address2: "2 East", city_town: "Greater Portmore", parish: "St Catherine"}; 
+  private get apiUrl() : string {
+    return `http://api.positionstack.com/v1/forward?access_key=${this.mapkey}&query=${this.address.street_address + " " + this.address.street_address2 + " " + this.address.city_town + ", " + this.address.parish + ", Jamaica" }`;
+  }
+  
 
   constructor(private httpClient: HttpClient) {
     
    }
 
-   public sendGetRequest(){
+   public getMap(){
     return this.httpClient.get<any[]>(this.apiUrl)
   }
 }
