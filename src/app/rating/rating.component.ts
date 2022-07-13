b63ae9e7d78d3fcfc8752bab1e232466c281d527
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-07-12 21:52:38 -0500

    added star rating components to products

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,66 +1,56 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-rating',
-  templateUrl: './rating.component.html',
-  styleUrls: ['./rating.component.css']
+    selector: 'app-rating',
+    templateUrl: './rating.component.html',
+    styleUrls: ['./rating.component.css'],
 })
 export class RatingComponent implements OnInit {
+    @Input() starsArr: number[] = [];
+    stars: number[] = [1, 2, 3, 4, 5];
+    selectedValue: number = 1;
+    isMouseOver = true;
+    sum: number = 0;
+    avg: number = 0;
+    displayedValue!: number;
 
-  stars:number[]=[1, 2, 3, 4, 5];
-  @Input() starsArr:number[]=[];
-  selectedValue:number = 1;
-  isMouseOver = true;
-  sum:number = 0;
-  avg:number = 0;
-  displayedValue!: number;
+    countStar(star: number) {
+        this.sum = 0;
+        this.starsArr.push(this.selectedValue);
 
+        this.starsArr.forEach((el) => {
+            this.sum += el;
+        });
 
-  countStar(star:number){
-    this.sum = 0
-    this.starsArr.push(this.selectedValue)
-
-    this.starsArr.forEach((el)=>{
-      this.sum += el
-    })
-
-      console.log("the array is: " + this.starsArr)
-    
-      console.log("sum is " + this.sum);
-    this.avg = Math.ceil(this.sum / this.starsArr.length)
-      console.log("avg is " + this.avg);   
-    
-  }
-
-  removeStar(star:number){
-    this.isMouseOver = false;
-    this.selectedValue != star
-
-    this.sum -= this.selectedValue
-    console.log("The new sum is " + this.sum);
-    
-  }
-
-  addClass(star:number){
-    if(this.isMouseOver){
-      this.selectedValue = star;
+        this.avg = Math.ceil(this.sum / this.starsArr.length);
     }
-  }
 
-  removeClass(){
-    if(this.isMouseOver){
-      this.selectedValue = this.avg;
+    removeStar(star: number) {
+        this.isMouseOver = false;
+        this.selectedValue != star;
+        this.sum -= this.selectedValue;
     }
-  }
 
-  constructor() { }
+    addClass(star: number) {
+        if (this.isMouseOver) {
+            this.selectedValue = star;
+        }
+    }
 
-  ngOnInit(): void {
-    this.starsArr.forEach((el)=>{
-      this.sum += el
-    })
-    this.avg = this.sum / this.starsArr.length
-    this.selectedValue = this.avg
-  }
+    removeClass() {
+        if (this.isMouseOver) {
+            this.selectedValue = this.avg;
+        }
+    }
 
+    constructor() {}
+
+    ngOnInit(): void {
+        this.starsArr.forEach((el) => {
+            this.sum += el;
+        });
+
+        this.avg = this.sum / this.starsArr.length;
+        this.selectedValue = this.avg;
+    }
 }
